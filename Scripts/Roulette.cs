99740e2265b3110c85c16242302e11c51ddafea1
IlyaVodop <Ilyavodop1@gmail.com>
2020-12-19 23:44:59 +0300

    first commit

---
+++ Scripts/Roulette.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public static class Roulette
+{
+    /// <summary>
+    /// <para>Принимает в аргументы массив шансов. Возвращает индекс элемента, который выпал. </para>
+    /// </summary>
+    public static int GetRoulette(double[] chances)
+    {
+        double total = 0;
+        foreach (var element in chances)
+        {
+            total = total + element;
+        }
+        double chance = Random.Range(0, 100) + 1;
+        double current = 0;
+        for (int index = 0; index < chances.Length; index++)
+        {
+            var pair = chances[index];
+            current = current + pair / total * 100;
+            if (chance <= current)
+            {
+                return index;
+            }
+        }
+        return 0;
+    }
+
+}
---
+++ Scripts/UiManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiManager : MonoBehaviour
+{
+    [SerializeField] private Button _game;
+    [SerializeField] private InputField _inputRed;
+    [SerializeField] private InputField _inputBlack;
+    [SerializeField] private Text _winText;
+    void Start()
+    {
+        _game.onClick.AddListener(() =>
+        {
+            string textRed = _inputRed.text;
+            double.TryParse(textRed,out double chanceRed);
+
+            string textBlack = _inputBlack.text;
+            double.TryParse(textBlack, out double chanceBlack);
+
+            if (chanceBlack == 0 || chanceRed == 0)
+            {
+                _winText.text = "Одно из значений заполнено некорректно или равно нулю";
+                return;
+            }
+
+            double[] chances = new[] { chanceRed, chanceBlack };
+            int value = Roulette.GetRoulette(chances);
+            
+            if (value == 0)
+            {
+                _winText.text = "Выпало красное";
+            }
+            else
+            {
+                _winText.text = "Выпало черное";
+            }
+        });
+    }
+
+}
